8b22779531fc915f2f5296ef44197f15d7d3d04d
GustavoSirkis <gustacosirkis@gmail.com>
2023-03-21 17:23:00 -0300

    update the server url

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data from server -> bot's response
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codex-ck9q.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-type': 'application/json'
